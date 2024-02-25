438b145414956bad44255e0b56d07f71a1816fe3
MohamedAklamaash <aklamaash78@gmail.com>
2024-02-25 23:24:43 +0530

    ts files uploaded

--- src/index.js
+++ src/index.js
@@ -9,14 +9,17 @@ const socket_io_1 = require("socket.io");
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const server = (0, http_1.createServer)(app);
+const frontendURL = app.get('env') === 'development' ? 'http://localhost:3000' : 'https://sketck-book.vercel.app/';
 const io = new socket_io_1.Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: frontendURL,
         credentials: true,
     },
     allowEIO3: true
 });
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: frontendURL
+}));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 io.on("connection", (socket) => {
