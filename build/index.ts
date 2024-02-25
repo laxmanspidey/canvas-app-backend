3060b529623cfea564da15ada87eeb3cf5274abb
MohamedAklamaash <aklamaash78@gmail.com>
2024-02-25 23:06:23 +0530

    backend done

--- build/index.ts
+++ build/index.ts
@@ -4,11 +4,11 @@ import {Server,Socket} from "socket.io"
 import cors from "cors"
 const app = express();
 const server = createServer(app);
-
+const frontendURL = process.env.NODE_ENV === "production" ? "https://sketck-book.vercel.app" :"http://localhost:3000"
 const io = new Server(server,
     {
         cors:{
-            origin:"http://localhost:3000",
+            origin:frontendURL,
             credentials:true,
         },
         allowEIO3: true
